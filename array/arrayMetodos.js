00f936cd2e34b2adcb253e954e64d449fd3ce8d7
Flávio Félix <fau_felix30@hotmail.com>
2020-11-10 22:00:15 -0300

    Feito aulas sobre arrays

---
+++ array/arrayMetodos.js
@@ -0,0 +1,27 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop()//Mass quebrou o carro!
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift()//remove o primeiro!
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')//Adiciona no primeiro
+console.log(pilotos)
+
+//splice pode adicionar e remover elementos
+//adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+//remover
+pilotos.splice(3, 1)//Massa quebrou :(
+console.log(pilotos)  
+
+const algunsPilotos1 = pilotos.slice(2)//Novo array
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
