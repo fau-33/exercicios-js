81df7821e6271464bcf88c3a1c2794701b4409f5
Flávio Félix <fau_felix30@hotmail.com>
2020-11-19 20:40:08 -0300

    Feito mais três exercicios map e filter

---
+++ array/map3.js
@@ -0,0 +1,23 @@
+Array.prototype.map2 = function(callback){
+  const newArray = []
+  for(let i = 0; i < this.length; i++){
+    newArray.push(callback(this[i], i, this))
+  }
+  return newArray
+}
+
+const carrinho = [
+  '{"nome": "Borracha", "preco": 3.45}',
+  '{"nome": "Caderno", "preco": 19.40}',
+  '{"nome": "Kit de Lápis", "preco": 41.22}',
+  '{"nome": "Caneta", "preco": 7.50}'
+
+]
+
+//Retonar arrays como os preços
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco 
+
+const resultado = carrinho.map2(paraObjeto).map(apenasPreco)
+console.log(resultado)
